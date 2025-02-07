8a981fbed087bc5bb9304e68a42ef0b17e36f14b
oskaitu <oska@itu.dk>
2025-02-07 13:54:20 +0100

    push another template

--- vite-project/src/App.tsx
+++ vite-project/src/App.tsx
@@ -1,4 +1,5 @@
 import {TimeLine} from './TimeLine.tsx'
+import {WhatEver} from './WhatEver.tsx'
 import './App.css'
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
@@ -8,6 +9,7 @@ function App() {
     <BrowserRouter>
 		<Routes>
 			<Route path="/" element={<TimeLine />} />
+			<Route path="/whatever" element={<WhatEver />} />
 		</Routes>
 	</BrowserRouter>
   )
