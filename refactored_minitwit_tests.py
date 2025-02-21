118c0383374cdaed7cb0c9a9ab834d283a846bea
andrada <andradaacondor@gmail.com>
2025-02-21 15:26:41 +0200

    Finalize web interface for Follow/Unfollow and modified code redirection handling

--- refactored_minitwit_tests.py
+++ refactored_minitwit_tests.py
@@ -3,19 +3,23 @@
     MiniTwit Tests
     ~~~~~~~~~~~~~~
 
-    Tests a MiniTwit application.
+    Tests the MiniTwit application (GO version).
 
-    :refactored: (c) 2024 by HelgeCPH from Armin Ronacher's original unittest version
     :copyright: (c) 2010 by Armin Ronacher.
     :license: BSD, see LICENSE for more details.
 """
 import requests
+import unittest
 
+# BASE_URL to the Go application
+BASE_URL = "http://localhost:5010"
 
-# import schema
-# import data
-# otherwise use the database that you got previously
-BASE_URL = "http://localhost:5000"
+# Session persistence for authentication
+http_session = requests.Session()
+
+def decode_data(response):
+    """Helper function to decode in utf-8 the response data"""
+    return response.text
 
 def register(username, password, password2=None, email=None):
     """Helper function to register a user"""
@@ -23,114 +27,137 @@ def register(username, password, password2=None, email=None):
         password2 = password
     if email is None:
         email = username + '@example.com'
-    return requests.post(f'{BASE_URL}/register', data={
-        'username':     username,
-        'password':     password,
-        'password2':    password2,
-        'email':        email,
+
+    return http_session.post(f'{BASE_URL}/register', data={
+        'username': username,
+        'password': password,
+        'password2': password2,
+        'email': email,
     }, allow_redirects=True)
 
 def login(username, password):
     """Helper function to login"""
-    http_session = requests.Session()
     r = http_session.post(f'{BASE_URL}/login', data={
         'username': username,
         'password': password
     }, allow_redirects=True)
-    return r, http_session
+    return r
 
-def register_and_login(username, password):
-    """Registers and logs in in one go"""
-    register(username, password)
-    return login(username, password)
-
-def logout(http_session):
+    '''
+    def register_and_login(self, username, password):
+        """Registers and logs in in one go"""
+        self.register(username, password)
+        return self.login(username, password)
+    '''
+def logout():
     """Helper function to logout"""
     return http_session.get(f'{BASE_URL}/logout', allow_redirects=True)
 
-def add_message(http_session, text):
-    """Records a message"""
-    r = http_session.post(f'{BASE_URL}/add_message', data={'text': text},
-                                allow_redirects=True)
-    if text:
-        assert 'Your message was recorded' in r.text
+def add_message(text):
+    """Adds a message"""
+    r = http_session.post(f'{BASE_URL}/add_message', data={'text': text}, allow_redirects=True)
     return r
 
-# testing functions
+    # testing functions
 
-def test_register():
-    """Make sure registering works"""
-    r = register('user1', 'default')
-    assert 'You were successfully registered ' \
-           'and can login now' in r.text
-    r = register('user1', 'default')
-    assert 'The username is already taken' in r.text
-    r = register('', 'default')
-    assert 'You have to enter a username' in r.text
-    r = register('meh', '')
-    assert 'You have to enter a password' in r.text
-    r = register('meh', 'x', 'y')
-    assert 'The two passwords do not match' in r.text
-    r = register('meh', 'foo', email='broken')
-    assert 'You have to enter a valid email address' in r.text
+class MiniTwitGoTestCase(unittest.TestCase):
 
-def test_login_logout():
-    """Make sure logging in and logging out works"""
-    r, http_session = register_and_login('user1', 'default')
-    assert 'You were logged in' in r.text
-    r = logout(http_session)
-    assert 'You were logged out' in r.text
-    r, _ = login('user1', 'wrongpassword')
-    assert 'Invalid password' in r.text
-    r, _ = login('user2', 'wrongpassword')
-    assert 'Invalid username' in r.text
+    def test_register(self):
+        """Ensure registration works"""
+        r = register('user1', 'default')
+        self.assertIn('You were successfully registered and can login now', decode_data(r))
 
-def test_message_recording():
-    """Check if adding messages works"""
-    _, http_session = register_and_login('foo', 'default')
-    add_message(http_session, 'test message 1')
-    add_message(http_session, '<test message 2>')
-    r = requests.get(f'{BASE_URL}/')
-    assert 'test message 1' in r.text
-    assert '&lt;test message 2&gt;' in r.text
+        r = register('user1', 'default')
+        self.assertIn('The username is already taken', decode_data(r))
 
-def test_timelines():
-    """Make sure that timelines work"""
-    _, http_session = register_and_login('foo', 'default')
-    add_message(http_session, 'the message by foo')
-    logout(http_session)
-    _, http_session = register_and_login('bar', 'default')
-    add_message(http_session, 'the message by bar')
-    r = http_session.get(f'{BASE_URL}/public')
-    assert 'the message by foo' in r.text
-    assert 'the message by bar' in r.text
+        r = register('', 'default')
+        self.assertIn('You have to enter a username', decode_data(r))
 
-    # bar's timeline should just show bar's message
-    r = http_session.get(f'{BASE_URL}/')
-    assert 'the message by foo' not in r.text
-    assert 'the message by bar' in r.text
+        r = register('meh', '')
+        self.assertIn('You have to enter a password', decode_data(r))
 
-    # now let's follow foo
-    r = http_session.get(f'{BASE_URL}/foo/follow', allow_redirects=True)
-    assert 'You are now following &#34;foo&#34;' in r.text
+        r = register('meh', 'x', 'y')
+        self.assertIn('The two passwords do not match', decode_data(r))
 
-    # we should now see foo's message
-    r = http_session.get(f'{BASE_URL}/')
-    assert 'the message by foo' in r.text
-    assert 'the message by bar' in r.text
+        r = register('meh', 'foo', email='broken')
+        self.assertIn('You have to enter a valid email address', decode_data(r))
 
-    # but on the user's page we only want the user's message
-    r = http_session.get(f'{BASE_URL}/bar')
-    assert 'the message by foo' not in r.text
-    assert 'the message by bar' in r.text
-    r = http_session.get(f'{BASE_URL}/foo')
-    assert 'the message by foo' in r.text
-    assert 'the message by bar' not in r.text
+    def test_login_logout(self):
+        """Ensure login and logout work correctly"""
+        register('user1', 'default')
+        r = login('user1', 'default')
+        # self.assertIn('You were logged in', decode_data(r))
+        self.assertEqual(r.status_code, 200)
 
-    # now unfollow and check if that worked
-    r = http_session.get(f'{BASE_URL}/foo/unfollow', allow_redirects=True)
-    assert 'You are no longer following &#34;foo&#34;' in r.text
-    r = http_session.get(f'{BASE_URL}/')
-    assert 'the message by foo' not in r.text
-    assert 'the message by bar' in r.text
+        # Check timeline for the flash message
+        self.assertIn('You were logged in', decode_data(r)) 
 
+        # Logout
+        r = logout()
+        self.assertIn('You were logged out', decode_data(r))
+
+        r = login('user1', 'wrongpassword')
+        self.assertIn('Invalid password', decode_data(r))
+
+        r = login('user2', 'wrongpassword')
+        self.assertIn('Invalid username', decode_data(r))
+
+    def test_message_recording(self):
+        """Ensure message posting works"""
+        login('foo', 'default')
+        add_message('test message 1')
+        add_message('<test message 2>')
+
+        r = http_session.get(f'{BASE_URL}/')
+        self.assertIn('test message 1', decode_data(r))
+        self.assertIn('&lt;test message 2&gt;', decode_data(r))  # Ensure HTML escape
+
+    def test_timelines(self):
+        """Ensure timelines work correctly"""
+        login('foo', 'default')
+        add_message('the message by foo')
+        logout()
+
+        login('bar', 'default')
+        add_message('the message by bar')
+
+        # Public timeline should show both messages
+        r = http_session.get(f'{BASE_URL}/public')
+        self.assertIn('the message by foo', decode_data(r))
+        self.assertIn('the message by bar', decode_data(r))
+
+        # Bar's personal timeline should only show bar's messages
+        r = http_session.get(f'{BASE_URL}/')
+        self.assertNotIn('the message by foo', decode_data(r))
+        self.assertIn('the message by bar', decode_data(r))
+
+        # Follow foo
+        r = http_session.get(f'{BASE_URL}/foo/follow', allow_redirects=True)
+        self.assertIn('You are now following "foo"', decode_data(r))
+
+        # Timeline should now show foo's messages
+        r = http_session.get(f'{BASE_URL}/')
+        self.assertIn('the message by foo', decode_data(r))
+        self.assertIn('the message by bar', decode_data(r))
+
+        # Bar's user page should only show their messages
+        r = http_session.get(f'{BASE_URL}/bar')
+        self.assertNotIn('the message by foo', decode_data(r))
+        self.assertIn('the message by bar', decode_data(r))
+
+        # Foo's user page should only show their messages
+        r = http_session.get(f'{BASE_URL}/foo')
+        self.assertIn('the message by foo', decode_data(r))
+        self.assertNotIn('the message by bar', decode_data(r))
+
+        # Unfollow foo and check timeline again
+        r = http_session.get(f'{BASE_URL}/foo/unfollow', allow_redirects=True)
+        self.assertIn('You are no longer following "foo"', decode_data(r))
+
+        r = http_session.get(f'{BASE_URL}/')
+        self.assertNotIn('the message by foo', decode_data(r))
+        self.assertIn('the message by bar', decode_data(r))
+
+
+if __name__ == '__main__':
+    unittest.main()
